979a701c0f13866617362f2e074b3049447c77d6
Silvio Petriconi <3768672+silviopetriconi@users.noreply.github.com>
2019-10-15 06:54:50 +0200

    fixed many small bugs

--- demochain/blockchain.py
+++ demochain/blockchain.py
@@ -10,8 +10,12 @@ import binascii
 import json
 from utils import merkle
 
-GENESIS_ROOT_DATA = [binascii.unhexlify('B0CC04IB0CC04I')]
+GENESIS_ROOT_DATA = [binascii.unhexlify('B0CC041B0CC041')]
 GENESIS_ROOT_NONCE = binascii.unhexlify('deadbeef')
+
+# Every block is a namedtuple, immutability is automatically enforced.
+Block = namedtuple('Block', \
+    'height prev timestamp difficulty merkleroot nonce')
 
 
 class Blockchain:
@@ -19,7 +23,7 @@ class Blockchain:
         self._blocks = []        # list of actual blocks
         self._blockindex = {}    # dict that maps hash to block index
         self._hashfunc = hashfunc if hashfunc else lambda x : hashlib.sha256(x).digest()
-        self._difficulty = 0xdeafbeef   # FIXME: implement difficulty later
+        self._difficulty = 0xdeadbeef   # FIXME: implement difficulty later
         self._append_block(self._create_genesis_block()) # append genesis block, no check
 
     def _create_genesis_block(self):
@@ -37,15 +41,11 @@ class Blockchain:
         :returns: the new block (without appending it to the chain)
         '''
         # compute hash of previous block
-        if len(self._blocks > 0):
+        if len(self._blocks) > 0:
             prevhash = self._hashfunc(self._serialize(self._blocks[-1]))
         else:
             # Genesis block has hash over empty set as previous field.
             prevhash = self._hashfunc(b'')
-            
-        # Block is namedtuple, immutability is automatically enforced.
-        Block = namedtuple('Block', \
-            'height prev timestamp difficulty merkleroot nonce')
 
         thisBlock = Block(
             height=len(self._blocks) + 1, \
@@ -57,12 +57,10 @@ class Blockchain:
         return thisBlock
 
     def _serialize(self, block):
-        # serializes a block to binary format. We're not
-        # caring about endianness here so this is not portable
-        # but it's good enough for demo.
-        
-        
-        pass
+        # serializes a block to disk-storable format.
+        # for now, we dump to JSON with alphabetically sorted keys.
+        # We can implement a more efficient storage form later. 
+        return json.dumps(block, sort_keys=True) 
 
     def _append_block(self, block):
         # actually appends block without checking it
@@ -72,8 +70,40 @@ class Blockchain:
 
         # if all checkes go through, save block hash in a dict
         self._blocks.append(block)
-        self._blockindex(blockhash) = len(self._blocks) 
+        self._blockindex[blockhash] = len(self._blocks) 
         return len(self._blocks)
+
+    def can_append(self, block, diagnostics=False):
+        '''
+        Checks whether a candidate block is valid and can be
+        appended to the chain. 
+        :param block: candidate block to be verified
+        :param diagnostics: if set True, returns diagnostics
+        :returns: True if block is valid, False otherwise. If
+        diagnostics is True, returns a tuple (valid, errorstring).
+        '''
+        if block.height != len(self._blocks) + 1 or \
+            block.prev != self._hashfunc(self._serialize(self._blocks[-1])):
+            return (False, "Bad block: block height or prev block invalid.") \
+                if diagnostics else False
+        
+        if block.prev not in self._blockindex:
+            return(False, "Bad prev hash pointer: prev block not in chain.") \
+                if diagnostics else False
+
+        if self._blockindex[block.prev] != len(self._blocks):
+            return(False, "Can't insert block: prev isn't at end of chain.") \
+                if diagnostics else False
+        
+        # then, check that difficulty is set appropriately
+        if self._difficulty != block.difficulty:
+            return(False, "Block difficulty is not at current level.") \
+                if diagnostics else False
+
+        if not self._blockhash_matches_difficulty(block):
+            raise ValueError("Block hash POW not commensurate to difficulty.")
+        
+        return (True,'') if diagnostics else True
 
 
     def append_block(self, block):
@@ -86,25 +116,14 @@ class Blockchain:
 
         # first, validate that the block height is n+1 and
         # that prev hash points to the last block on chain
-        if block.height != len(self._blocks) + 1 or \
-            block.prev != self._hashfunc(self._serialize(self.blocks[-1])):
-            raise ValueError("Bad block: height or prev fields invalid.")
-        
-        if block.prev not in self._blockindex:
-            raise ValueError("Bad prev hash pointer: prev block not in chain.")
-        if self._blockindex[block.prev] != len(self._blocks):
-            raise ValueError("Can't insert block: prev isn't at end of chain.")
-        
-        # then, check that difficulty is set appropriately
-        if self._difficulty != block.difficulty:
-            raise ValueError("Block difficulty is not at current level.")
+        can_append, errormsg = self.can_append(block, diagnostics=True)
 
-        if not self._blockhash_matches_difficulty(block):
-            raise ValueError("Block hash POW not adequate to difficulty.")
+        if not can_append:
+            raise ValueError(errormsg)
 
         return self._append_block(block)
 
-    def _blockhash_matches_difficulty(self, someblock):
+    def _blockhash_matches_difficulty(self, block):
         # return TRUE if the hash matches the difficulty indicated in
         # the block
         # FIXME: we need to implement difficulty carefully. For now, four 
@@ -130,3 +149,16 @@ class Blockchain:
         pass
 
 
+
+
+
+if __name__=='__main__':
+    B = Blockchain()
+    nonce = 0
+    while True:
+        candidate = B.create_block(0xdeafbeef, nonce)
+        if B.can_append(candidate): # mining successsful
+            break
+        nonce += 1
+    B.append_block(candidate)
+    print("Success!")
